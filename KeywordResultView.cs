9f5935b430687dff4c8a711aba26bb41ca94b0dc
windchimes91 <102124038+windchimes91@users.noreply.github.com>
2025-05-02 00:25:06 +0800

    Add files via upload

---
+++ KeywordResultView.cs
@@ -0,0 +1,8 @@
+﻿namespace TPMRTweb.Models
+{
+    public class KeywordResultView
+    {
+        public List<KeywordStatisticPerDay> StatisticsPerDay { get; set; } = new();
+        public Dictionary<string, string> KeywordDisplayNames { get; set; } = new();
+    }
+}
